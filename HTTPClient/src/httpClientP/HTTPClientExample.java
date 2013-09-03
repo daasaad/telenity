bb4238b9b9e52620f4603965147ec663dc09c4da
saadtelenity <saadettins@saadettins-lnx>
2013-09-03 14:45:38 +0300

    HTTPClientExample

--- HTTPClient/src/httpClientP/HTTPClientExample.java
+++ HTTPClient/src/httpClientP/HTTPClientExample.java
@@ -12,37 +12,37 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 public class HTTPClientExample {
 
-public static void main(String[] args) throws Exception {
-	HttpClient client = new DefaultHttpClient();
-  HttpGet request = new HttpGet("http://localhost:8080/HelloWorldServlet/servlet?user=ssad&passwd=eferferf&msisdn=0394034993&text=dsdfkdkfsdsdfsdfadsaafaf");
+	public static void main(String[] args) throws Exception {
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(
+				"http://localhost:8080/HelloWorldServlet/servlet?user=ssad&passwd=eferferf&msisdn=0394034993&text=dsdfkdkfsdsdfsdfadsaafaf");
 
-HttpResponse response = null;
-try {
-	response = client.execute(request);
-} catch (ClientProtocolException e) {
-	e.printStackTrace();
-} catch (IOException e) {
-	e.printStackTrace();
-}
+		HttpResponse response = null;
+		try {
+			response = client.execute(request);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-BufferedReader rd = null;
-try {
-	rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
-} catch (IllegalStateException e1) {
-	e1.printStackTrace();
-} catch (IOException e1) {
-	e1.printStackTrace();
-}
+		BufferedReader rd = null;
+		try {
+			rd = new BufferedReader(new InputStreamReader(response.getEntity()
+					.getContent()));
+		} catch (IllegalStateException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 
-String line = "";
-try {
-	while ((line = rd.readLine()) != null)
-	  System.out.println(line);
-} catch (IOException e) {
-	e.printStackTrace();
-}
-	
+		String line = "";
+		try {
+			while ((line = rd.readLine()) != null)
+				System.out.println(line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-
-}
+	}
 }
